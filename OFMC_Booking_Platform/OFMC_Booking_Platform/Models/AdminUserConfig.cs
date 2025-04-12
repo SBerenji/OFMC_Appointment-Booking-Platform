cb87d00460c116824573b2ec6bd5f7d27f98d1b9
Sudhan <86077071+Sudhan-Dahake@users.noreply.github.com>
2025-04-12 09:48:42 -0400

    Migrated from .env for storing admin credentials to storing them in appsetiings.Development.json.

---
+++ OFMC_Booking_Platform/OFMC_Booking_Platform/Models/AdminUserConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace OFMC_Booking_Platform.Models
+{
+    public class AdminUserConfig
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? DOB { get; set; }
+    }
+}
